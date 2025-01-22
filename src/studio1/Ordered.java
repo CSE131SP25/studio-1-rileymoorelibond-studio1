4b9ee8e7e6860466796f4cfc07c47a9c02b97185
86187 <86187@LAPTOP-G3P14HDO>
2025-01-22 14:06:28 -0600

    .

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,6 +15,12 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
+		
+		boolean result1 = (x>y && y>z);
+		boolean result2 = (x<y && y<z);
+		System.out.print(result1 && result2);
+		System.out.println();
+		System.out.print(result1 || result2);
 
 	}
 
